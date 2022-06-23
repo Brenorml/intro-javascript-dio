56dfd8cf218386476b59fbcf4cda0ca2550fbad8
Brenorml <breno.lucena@gmail.com>
2022-06-23 22:53:49 +0100

    Starting JavaScript studies - sintax, parameters, atributes, var and const

---
+++ teste.js
@@ -0,0 +1,13 @@
+//Exemplo 1 - variaveis e constantes
+//var preco = 2;
+var desconto = 0.2;
+const PRECO = 2;
+var total = PRECO - desconto; 
+
+
+//Exemplo 2 - parametros, sintaxe, função, invocação
+function soma(a, b) {    
+    return a + b; //8
+}
+
+soma(3, 5);
