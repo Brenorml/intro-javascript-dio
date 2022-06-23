85ade08a50248f286693aa7daa4920b26d58ccd7
Brenorml <breno.lucena@gmail.com>
2022-06-23 23:32:59 +0100

    Installing and testing - Node JS (console) + extracting even numbers from the list

--- teste.js
+++ teste.js
@@ -1,13 +1,31 @@
 //Exemplo 1 - variaveis e constantes
 //var preco = 2;
-var desconto = 0.2;
+/* var desconto = 0.2;
 const PRECO = 2;
-var total = PRECO - desconto; 
+var total = PRECO - desconto; */
 
 
 //Exemplo 2 - parametros, sintaxe, função, invocação
-function soma(a, b) {    
+/* function soma(a, b) {    
     return a + b; //8
 }
 
-soma(3, 5);
+soma(3, 5); */
+
+//console.log("Hello world!");
+
+function returnEvenValues(array) {
+    let evenNums = [];
+    for(let i = 0; i < array.length; i++) {
+      if(array[i] % 2 === 0) {
+        evenNums.push(array[i]);
+      } else {
+        console.log(`${array[i]} não é par`);
+      }
+    }
+    console.log('os números pares são:',evenNums);
+}
+
+let array = [1, 2, 4, 5, 7, 8];
+
+returnEvenValues(array);
