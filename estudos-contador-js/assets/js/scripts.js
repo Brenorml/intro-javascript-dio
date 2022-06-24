662efaf5b8af3f2cfbb1c6789cc7e74936166268
Brenorml <breno.lucena@gmail.com>
2022-06-25 00:52:34 +0100

    Making the counter + added increment limit + red color while count under zero

---
+++ estudos-contador-js/assets/js/scripts.js
@@ -0,0 +1,21 @@
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    if (currentNumber > 10) {
+        currentNumber = 10;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }
+    if (currentNumber >= 0)
+        document.getElementById("currentNumber").style.color = "black";
+
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    if (currentNumber < 0)
+        document.getElementById("currentNumber").style.color = "red";
+}
