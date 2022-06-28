a6d3417e4c9bf2aa28186776081ccfdc709fceff
Brenorml <breno.lucena@gmail.com>
2022-06-28 11:38:33 +0100

    directory name changed

---
+++ estudos-var-let-const-js/script.js
@@ -0,0 +1,24 @@
+/* let numberOne;
+
+ numberOne = 1;
+
+ console.log(numberOne + 2); */
+
+ /* var firstName = 'João';
+ let lastName = 'Souza'; //escopo global
+
+ if(firstName === 'João') {
+    var firstName = 'Pedro';
+    let lastName = 'Silva'; //escopo local do bloco   
+
+    console.log(firstName, lastName);
+ }
+
+ console.log(firstName, lastName); */
+
+const FIRST_NAME = "Breno";
+
+console.log(FIRST_NAME);
+
+
+
