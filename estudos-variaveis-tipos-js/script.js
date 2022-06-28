b09b92d8ebebd8cd1d043f2288c435a3460e47fa
Brenorml <breno.lucena@gmail.com>
2022-06-28 11:14:46 +0100

    testing for scope differences

---
+++ estudos-variaveis-tipos-js/script.js
@@ -0,0 +1,18 @@
+/* let numberOne;
+
+ numberOne = 1;
+
+ console.log(numberOne + 2); */
+
+ var firstName = 'João';
+ let lastName = 'Souza'; //escopo global
+
+ if(firstName === 'João') {
+    var firstName = 'Pedro';
+    let lastName = 'Silva'; //escopo local do bloco   
+
+    console.log(firstName, lastName);
+ }
+
+ console.log(firstName, lastName);
+
